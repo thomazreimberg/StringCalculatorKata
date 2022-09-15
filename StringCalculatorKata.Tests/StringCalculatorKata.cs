769b16461f6bda24f06f65e4725b8dfa71570178
Thomaz Henrique <treimberg7@gmail.com>
2022-09-15 09:53:13 -0300

    Add project files.

---
+++ StringCalculatorKata.Tests/StringCalculatorKata.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace StringCalculatorKata.Tests
+{
+    public class StringCalculatorKata
+    {
+        [Test]
+        public void Add_EmptyStringAsParam_ReturnsZero()
+        {
+            Assert.AreEqual(0, StringCalculator.Add(string.Empty));
+        }
+
+        [Test]
+        public void Add_StringContainingSingleNumber_ReturnsTheNumberItself()
+        {
+            Assert.AreEqual(5, StringCalculator.Add("5"));
+        }
+
+        [Test]
+        public void Add_TwoNumbersSeparatedByComma_ReturnsTheirSum()
+        {
+            var numbers = "7,8";
+            var expectedResult = 15;
+
+            Assert.AreEqual(expectedResult, StringCalculator.Add(numbers));
+        }
+
+        [Test]
+        public void Add_ThreeNumbersSeparatedByComma_ReturnsTheirSum()
+        {
+            var numbers = "1, 2, 3";
+            var expected = 6;
+
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase("1,2,3,4", 10)]
+        [TestCase("8,7,20", 35)]
+        [TestCase("5,0,4,1001", 9)]
+        [TestCase("5,0,4,1000", 1009)]
+        [TestCase("26,6,90", 122)]
+        public void Add_MoreThanThreeNumbersSeparatedByComma_ReturnsTheirSum(string input, int result)
+        {
+            Assert.AreEqual(result, StringCalculator.Add(input));
+        }
+
+        [TestCase("1,2,3,4,5,-5")]
+        [TestCase("-1,1,2,9")]
+        [TestCase("5,6,8,-5")]
+        public void Add_StringContainingNegativeNumbers_Throws(string numbers)
+        {
+            Assert.Throws<ArgumentException>(() => StringCalculator.Add(numbers));
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            var parts = numbers.Split(",");
+            var result = 0;
+            var negatives = new List<int>();
+
+            foreach (var part in parts)
+            {
+                int.TryParse(part, out int number);
+
+                if (number < 0)
+                    negatives.Add(number);
+                else if (number <= 1000)
+                    result += number;
+            }
+
+            if (negatives.Count > 0)
+            {
+                var negativeList = string.Join(',', negatives);
+                var exceptionMessage = $"Negative numbers not allowed: {negativeList}.";
+
+                throw new ArgumentException(exceptionMessage);
+            }
+
+            return result;
+        }
+    }
+}
